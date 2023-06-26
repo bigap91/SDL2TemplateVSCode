76b88778d4505cad2e07e68785d277994a0d64e7
bigap <papantonin@gmail.com>
2023-06-26 18:04:13 +0200

    master

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,30 @@
 //
 
 #include "my_header.h"
+#define SDL_MAIN_HANDLED
+#include "SDL/sdl.h"
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+    SDL_Event event;
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    bool running = true;
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_CLOSE)
+                    running = false;
+                break;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
 }
